128a4d571bf78cc4113774ab878fc745809db0bd
Eigel Asinas <eigelasinas1962@gmail.com>
2022-06-30 10:26:29 +0800

    Use Nobela's server.

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,9 @@
-use std::{env, process};
-
-use dialoguer::{Select, theme::ColorfulTheme, console::Term, Input};
-use nobela_parser2::{parse_flat, parse_nested, FlatStmt, NestedStmt};
-
+use device_query::{DeviceQuery, DeviceState, Keycode};
+use dialoguer::{console::Term, theme::ColorfulTheme, Select};
+use nobela_parser2::{parse_flat, server};
 
 fn main() {
-	let args: Vec<String> = env::args().collect();
-	let mode = args.get(1).unwrap_or_else(|| {
-        eprintln!("No argument for mode provided. Please enter either 'flat' or 'nested'");
-        process::exit(1);
-    });
-	
-	let input =
-	r#"
+    let input = r#"
 "Hey, there! This is a demo for Nobela!"
 "This is just a regular dialogue. Cool, huh?"
 "This one has choices!"
@@ -37,224 +28,107 @@ fn main() {
 "Well, that's about all we have for now..."
 	"#;
 
-	if args.len() > 1 {
-		match mode as &str {
-			"flat" => {
-				let events = parse_nested(input).unwrap_or_else(|e| panic!("{}", e));
-				execute_nested(&events);
-			},
-			"nested" => {
-				let events = parse_flat(input).unwrap_or_else(|e| panic!("{}", e));
-				execute_flat(&events);
-			},
-			_ => {
-				eprintln!("Invalid mode provided. Please enter either 'flat' or 'nested'");
-        		process::exit(1);
-			}
-		}
-	}
+    let stmts = parse_flat(input).unwrap_or_else(|e| panic!("{}", e));
+    let mut events = server::Server::new(&stmts, 0);
 
-	clear()
-	
-}
+    let mut e = events.next();
 
-fn execute_choice(choice: &NestedStmt) {
-    if let NestedStmt::Choice { children, .. } = choice {
-        for event in children {
-            match event {
-                NestedStmt::Dialogue { .. } => execute_dialogue(event),
-                NestedStmt::Choice { .. } => (),
-            }
-        }
-    }
-}
+    let mut just_pressed = Vec::new();
+    let mut pressed = Vec::new();
 
-fn execute_dialogue(dialogue: &NestedStmt) {
-    if let NestedStmt::Dialogue {
-        choices, ..
-    } = dialogue
-    {
-		show_dialogue_nested(dialogue);
-		
-		if choices.is_empty() {
-			wait_enter()
-		} else {
-			let choice_index = choose_nested(choices);
-			execute_choice(&choices[choice_index])
-		}
-    }
-}
-fn execute_nested(events: &Vec<NestedStmt>) {
-    for event in events {
+    while let Some(ref event) = e {
         match event {
-            NestedStmt::Dialogue { .. } => execute_dialogue(event),
-            NestedStmt::Choice { .. } => (),
-        }
-    }
-}
-
-fn execute_flat(events: &Vec<FlatStmt>) {
-    let mut index = 0;
-
-    while index < events.len() {
-		let event = &events[index];
-        match event {
-            FlatStmt::Dialogue { .. } => {
-                let mut next_index = index + 1;
-                let mut choices = Vec::new();
-                let mut choice_indexes = Vec::new();
-                let mut nested_count = 0;
-
-                loop {
-					let next_event = &events[next_index];
-                    match next_event {
-                        FlatStmt::EndDialogue => {
-                            if nested_count > 0 {
-                                nested_count -= 1
-                            } else {
-                                break;
-                            }
-                        }
-                        FlatStmt::Dialogue { .. } => nested_count += 1,
-                        FlatStmt::Choice { .. } => {
-                            if nested_count > 0 {
-                                nested_count += 1
-                            } else {
-                                choices.push(next_event);
-                                choice_indexes.push(next_index.to_owned())
-                            }
-                        }
-                        FlatStmt::EndChoice => {
-                            if nested_count > 0 {
-                                nested_count -= 1
-                            }
-                        } 
+            server::Event::Dialogue {
+                speaker,
+                text,
+                choices,
+            } => {
+                show_dialogue(speaker, text);
+                if !choices.is_empty() {
+                    let choice_index = choose(choices);
+                    events.choose(choice_index)
+                } else {
+                    enum Nav {
+                        Next,
+                        Back,
                     }
-                    next_index += 1;
-                }
+                    let mut nav: Option<Nav> = None;
 
-                show_dialogue_flat(&event);
+                    println!("<- Back		Next ->");
 
-				index = if choices.is_empty() {
-					wait_enter();
-					index + 1
-				} else {
-					choice_indexes[choose_flat(&choices)]
-				}
+                    while nav.is_none() {
+                        let device_state = DeviceState::new();
+                        let keys: Vec<Keycode> = device_state.get_keys();
 
-                // index = if !choice_indexes.is_empty() {
-                //     choice_indexes[0]
-                // } else {
-                //     index + 1
-                // }; // Change depending on user choice
-            }
-            FlatStmt::Choice { .. } => {
-                index += 1;
-            }
-            FlatStmt::EndDialogue => {
-                index += 1;
-            }
-            FlatStmt::EndChoice => {
-                let mut next_index = index + 1;
-                let mut nested_count = 0;
-				let mut next_event = &events[next_index];
+                        just_pressed.clear();
 
-                if matches!(next_event, FlatStmt::Choice { .. }) {
-                    loop {
-                        match next_event {
-                            FlatStmt::Dialogue { .. } => nested_count += 1,
-                            FlatStmt::EndDialogue => {
-                                if nested_count > 0 {
-                                    nested_count -= 1
-                                } else {
-                                    break;
-                                }
+                        let mut remove_indexes = Vec::new();
+
+                        for (i, key) in pressed.iter().enumerate() {
+                            if !keys.contains(key) {
+                                remove_indexes.push(i)
                             }
-                            FlatStmt::Choice { .. } => nested_count += 1,
-                            FlatStmt::EndChoice => nested_count -= 1,
                         }
-                        next_index += 1;
-						next_event = &events[next_index];
+
+                        for i in remove_indexes {
+                            pressed.remove(i);
+                        }
+
+                        for key in keys {
+                            if !pressed.contains(&key) {
+                                just_pressed.push(key);
+                                pressed.push(key);
+                            }
+                        }
+
+                        nav = if (just_pressed.contains(&Keycode::Right)
+                            || just_pressed.contains(&Keycode::Enter))
+                            && !just_pressed.contains(&Keycode::Left)
+                        {
+                            Some(Nav::Next)
+                        } else if just_pressed.contains(&Keycode::Left)
+                            && !(just_pressed.contains(&Keycode::Right)
+                                || just_pressed.contains(&Keycode::Enter))
+                        {
+                            Some(Nav::Back)
+                        } else {
+                            None
+                        };
                     }
                 }
-                index = next_index;
             }
+            server::Event::Ignore => (),
         }
+        e = events.next();
     }
+
+    clear()
 }
 
-
-fn wait_enter() {
-	Input::<String>::new()
-	.with_prompt("(Continue...)")
-	.allow_empty(true)
-	.report(false)
-    .interact_text().unwrap();
+fn show_dialogue(speaker: &Option<String>, text: &String) {
+    clear();
+    let output = if let Some(speaker) = speaker {
+        format!("{speaker}: {text}")
+    } else {
+        format!(": {text}")
+    };
+    println!("{output}\n");
 }
 
-fn choose_nested(choices: &Vec<NestedStmt>) -> usize {
-	let selection = Select::with_theme(&ColorfulTheme::default())
-        .items(&choices.into_iter().map(|c| to_string_nested(c)).collect::<Vec<String>>())
+fn choose(choices: &[String]) -> usize {
+    let selection = Select::with_theme(&ColorfulTheme::default())
+        .items(choices)
         .default(0)
-        .interact_on_opt(&Term::stderr()).unwrap();
+        .interact_on_opt(&Term::stderr())
+        .unwrap();
 
-	if let Some(selection) = selection {
-		selection
-	} else {
-		0
-	}
-}
-
-fn show_dialogue_nested(event: &NestedStmt) {
-	clear();
-	println!("{}\n", to_string_nested(event));
-}
-
-fn to_string_nested(event: &NestedStmt) -> String {
-	match event {
-		NestedStmt::Dialogue { speaker, text, .. } => {
-			if let Some(speaker) = speaker {
-				format!("{speaker}: {text}")
-			} else {
-				format!(": {text}")
-			}
-		},
-		NestedStmt::Choice { text, .. } => text.to_owned(),
-	}
-}
-
-fn choose_flat(choices: &Vec<&FlatStmt>) -> usize {
-	let selection = Select::with_theme(&ColorfulTheme::default())
-        .items(&choices.into_iter().map(|c| to_string_flat(c)).collect::<Vec<String>>())
-        .default(0)
-        .interact_on_opt(&Term::stderr()).unwrap();
-
-	if let Some(selection) = selection {
-		selection
-	} else {
-		0
-	}
-}
-
-fn show_dialogue_flat(event: &FlatStmt) {
-	clear();
-	println!("{}\n", to_string_flat(event));
-}
-
-fn to_string_flat(event: &FlatStmt) -> String {
-	match event {
-		FlatStmt::Dialogue { speaker, text, .. } => {
-			if let Some(speaker) = speaker {
-				format!("{speaker}: {text}")
-			} else {
-				format!(": {text}")
-			}
-		},
-		FlatStmt::Choice { text, .. } => text.to_owned(),
-		_ => String::new()
-	}
+    if let Some(selection) = selection {
+        selection
+    } else {
+        0
+    }
 }
 
 fn clear() {
-	print!("{}[2J", 27 as char);
+    print!("{}[2J", 27 as char);
 }
